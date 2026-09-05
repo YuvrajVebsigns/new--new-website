89783f640bf332eb9004e82c0d6a0829f9ac0cd7
YuvrajVebsigns <yuvraj.vebsigsn@vebsigns.com>
2026-09-05 19:33:59 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    cpus: 1,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cioangelnetwork.com' },
